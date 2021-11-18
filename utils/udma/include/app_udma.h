b37bdd0dfdb498023b05ff4ec77499ce1f96bba2
Lucas Weaver <l-weaver@ti.com>
2021-11-18 13:44:19 -0600

    [J721S2][UDMA] Adding UDMA inst for CSIRX/CSITX

--- utils/udma/include/app_udma.h
+++ utils/udma/include/app_udma.h
@@ -266,6 +266,34 @@ int32_t appUdmaDeInit(void);
  */
 void *appUdmaGetObj(void);
 
+#ifdef SOC_J721S2
+
+/**
+ *  \brief Performs initializations needed for UDMA BCDMA instance
+ *         Required for CSIRX/CSITX on J721S2
+ *
+ *  \return  0 incase of success else returns failure code
+ */
+int32_t appUdmaCsirxCsitxInit(void);
+
+/**
+ *  \brief Performs de-initializations needed for UDMA BCDMA instance
+ *         Required for CSIRX/CSITX on J721S2
+ *
+ *  \return  0 incase of success else returns failure code
+ */
+int32_t appUdmaCsirxCsitxDeInit(void);
+
+/**
+ *  \brief Provides global handle to UDMA Obj BCDMA instance
+ *         Required for CSIRX/CSITX on J721S2
+ *
+ *  \return  0 incase of success else returns failure code
+ */
+void *appUdmaCsirxCsitxGetObj(void);
+
+#endif
+
 /**
  *  \brief DMA copy init function.
  *
