d1026f1e51a77c8e4deee45fa4484a06e13b5964
Lucas Weaver <l-weaver@ti.com>
2021-11-17 21:14:07 -0600

    Revert "[PINMUX] Updating pinmux for I2C4"
    This reverts commit 16f2fa1f26be55115878f109dd370f04f2861c94.

--- utils/misc/src/app_pinmux_j721s2.c
+++ utils/misc/src/app_pinmux_j721s2.c
@@ -67,7 +67,6 @@
 #include <ti/board/board.h>
 #include <utils/misc/include/app_misc.h>
 #include <ti/board/src/j721s2_evm/include/board_pinmux.h>
-
 /* ========================================================================== */
 /*                           Macros & Typedefs                                */
 /* ========================================================================== */
@@ -128,8 +127,7 @@ static pinmuxPerCfg_t gI2c5PinCfg[] =
 
 static pinmuxModuleCfg_t gCaptPinCfg[] =
 {
-    {0, TRUE, gI2c4PinCfg},
-    {1, TRUE, gI2c5PinCfg},
+    {0, TRUE, gI2c5PinCfg},
     {PINMUX_END}
 };
 
