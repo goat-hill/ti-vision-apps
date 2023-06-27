6f9972e3063f1a2c676c9f30907f224cee2b58c6
Shiva Tripathi <s-tripathi1@ti.com>
2023-06-27 18:15:52 +0530

    [AM62A][QNX] removing double paranthesis leading to build error

--- platform/am62a/rtos/common/app_init.c
+++ platform/am62a/rtos/common/app_init.c
@@ -262,7 +262,7 @@ int32_t appInit()
     /* appGetIpcResourceTable() returns NULL in RTOS only mode and returns a valid resource table
      */
     ipc_init_prm.ipc_resource_tbl = ipc_resource_table;
-    if((host_os_type == APP_HOST_TYPE_LINUX))
+    if(host_os_type == APP_HOST_TYPE_LINUX)
     {
         ipc_init_prm.enable_tiovx_ipc_announce = 1;
     }
