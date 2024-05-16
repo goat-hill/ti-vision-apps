cffcf541bd8cd7b03d9275ed9d8a49a835730af8
Lucas Weaver <l-weaver@ti.com>
2024-05-16 10:04:43 -0500

    [I2C] Updating to the latest i2c header path

--- utils/dss/src/app_dss_soc.c
+++ utils/dss/src/app_dss_soc.c
@@ -44,8 +44,8 @@
 #include "app_dss_defaults_priv.h"
 #include <utils/sciclient/include/app_sciclient_wrapper_api.h>
 
-#include <i2c/I2C.h>
-#include <i2c/soc/I2C_soc.h>
+#include <i2c/i2c.h>
+#include <i2c/soc/i2c_soc.h>
 
 #include <ti/board/src/devices/board_devices.h>
 #include <ti/board/board.h>
